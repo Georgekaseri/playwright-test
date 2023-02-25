cbf87ba6c3c9afe96597ba0ca369511e00649653
Georgekaseri <georgekaseri22@gmail.com>
2023-02-25 23:21:49 +0000

    fix test

--- tests/electricFleet.spec.js
+++ tests/electricFleet.spec.js
@@ -23,23 +23,27 @@ test.describe("Verify Zenobe Electric Fleets page", () => {
   });
 
   test("should have the correct page title", async () => {
-    // expect(await page.title()).toBe("Electric Fleets | Zenobe");
+    expect(await page.title()).toBe("Electric Fleets - Zenobē");
   });
 
   test("should have a visible hero image", async () => {
-    //  const heroImage = await page.waitForSelector(".elementor-menu-toggle");
-    //expect(await heroImage.isVisible()).toBe(true);
-  });
-
-  test('should have a visible "Introduction" section', async () => {
-    //  const section = await page.waitForSelector(".electric-fleets-intro");
-    // expect(await section.isVisible()).toBe(true);
-  });
-
-  test('should have a visible "Our Expertise" section', async () => {
-    /*  const section = await page.waitForSelector(
-      ".elementor-container elementor-column-gap-no"
+    const heroImage = await page.waitForSelector(
+      "//img[@src='https://www.zenobe.com/wp-content/uploads/2022/06/2022-04-13_Zenobe_1044-1024x683.jpg']"
     );
-    // expect(await section.isVisible()).toBe(true);*/
+    expect(await heroImage.isVisible()).toBe(true);
+  });
+
+  test('should have a visible "Power procurement" section', async () => {
+    const section = await page.waitForSelector(
+      "//h2[contains(text(),'Power procurement')]"
+    );
+    expect(await section.isVisible()).toBe(true);
+  });
+
+  test('should have a visible "Minimising upfront costs" section', async () => {
+    const section = await page.waitForSelector(
+      "//h2[contains(text(),'Minimising upfront costs')]"
+    );
+    expect(await section.isVisible()).toBe(true);
   });
 });
